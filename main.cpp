a5880a40aef829bef94864b541ebe6efb1d3b9ec
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2024-08-12 19:23:46 -0300

    it works on 'runGame' function

--- main.cpp
+++ main.cpp
@@ -34,6 +34,12 @@ public:
 
   Unit(const string &tittle, int coins, int diamonds, int rubies, int id)
       : id(id), tittle(tittle), thisValue{coins, diamonds, rubies} {}
+
+  void introduceItself()
+      const
+  {
+    cout << id << " - " << tittle << " ( " << "Coins: " << thisValue.coins << ", Diamonds: " << thisValue.diamonds << ", Rubies: " << thisValue.rubies << " ) " << endl;
+  }
 };
 
 class Earning : public Unit
@@ -51,55 +57,55 @@ public:
 };
 
 bool checkTxtFile(const string &txtFilename);
-void introduceTheGame();
 void createTxtFile();
 void readTxtFile();
+void introduceTheGame();
 void runGame();
 void reWriteTxtFile();
 
+array<Earning, EARNING_ARR_SIZE> earningGroup = {
+    Earning("Mental revision", 5, 0, 0, 1),
+    Earning("Written revision", 30, 0, 0, 2),
+    Earning("Digital revision", 20, 0, 0, 3),
+    Earning("Watch video lesson ( 0 - 5 min )", 5, 0, 0, 4),
+    Earning("Watch video lesson ( 6 - 15 min )", 10, 1, 0, 5),
+    Earning("Watch video lesson ( 16 - 30 min )", 15, 2, 1, 6),
+    Earning("Watch video lesson ( 31 - 59 min )", 30, 4, 2, 7),
+    Earning("Watch video lesson ( 1 hour - ++ )", 60, 10, 4, 8),
+    Earning("Teach someone", 10, 0, 0, 9),
+    Earning("Read an article", 10, 0, 0, 10),
+    Earning("Read a ChatGPT explanation", 5, 0, 0, 11),
+    Earning("Read a documentation", 15, 0, 1, 12),
+    Earning("Written exercise ( Easy )", 6, 0, 0, 13),
+    Earning("Digital exercise ( Easy )", 5, 0, 0, 14),
+    Earning("Written exercise ( Medium )", 11, 0, 0, 15),
+    Earning("Digital exercise ( Medium )", 10, 0, 0, 16),
+    Earning("Written exercise ( Hard )", 16, 0, 1, 17),
+    Earning("Digital exercise ( Hard )", 15, 0, 1, 18),
+};
+
+array<Spending, SPENDING_ARR_SIZE> spendingGroup = {
+    Spending("Free day", 0, 100, 0, 19),
+    Spending("Free weekend", 50, 0, 120, 20),
+    Spending("Short break ( 10 min )", 15, 0, 0, 21),
+    Spending("Short break+ ( 15 min )", 20, 0, 0, 22),
+    Spending("Medium break ( 20 min )", 25, 0, 0, 23),
+    Spending("Medium break+ ( 30 min )", 35, 0, 0, 24),
+    Spending("The break ( 1 hour )", 60, 0, 0, 25),
+};
+
 int main()
 {
-
   bool isThereFile = checkTxtFile(txtFileName);
 
   if (!isThereFile)
   {
-    introduceTheGame();
     createTxtFile();
   }
 
   readTxtFile();
 
-  array<Earning, EARNING_ARR_SIZE> earningGroup = {
-      Earning("1 - Mental revision", 5, 0, 0, 1),
-      Earning("2 - Written revision", 30, 0, 0, 2),
-      Earning("3 - Digital revision", 20, 0, 0, 3),
-      Earning("4 - Watch video lesson ( 0 - 5 min )", 5, 0, 0, 4),
-      Earning("5 - Watch video lesson ( 6 - 15 min )", 10, 1, 0, 5),
-      Earning("6 - Watch video lesson ( 16 - 30 min )", 15, 2, 1, 6),
-      Earning("7 - Watch video lesson ( 31 - 59 min )", 30, 4, 2, 7),
-      Earning("8 - Watch video lesson ( 1 hour - ++ )", 60, 10, 4, 8),
-      Earning("9 - Teach someone", 10, 0, 0, 9),
-      Earning("10 - Read an article", 10, 0, 0, 10),
-      Earning("11 - Read a ChatGPT explanation", 5, 0, 0, 11),
-      Earning("12 - Read a documentation", 15, 0, 1, 12),
-      Earning("13 - Written exercise ( Easy )", 6, 0, 0, 13),
-      Earning("14 - Digital exercise ( Easy )", 5, 0, 0, 14),
-      Earning("15 -Written exercise ( Medium )", 11, 0, 0, 15),
-      Earning("16 Digital exercise ( Medium )", 10, 0, 0, 16),
-      Earning("17 - Written exercise ( Hard )", 16, 0, 1, 17),
-      Earning("18 - Digital exercise ( Hard )", 15, 0, 1, 18),
-  };
-
-  array<Spending, SPENDING_ARR_SIZE> spendingGroup = {
-      Spending("19 - Free day", 0, 100, 0, 19),
-      Spending("20 - Free weekend", 50, 0, 120, 20),
-      Spending("21 - Short break ( 10 min )", 15, 0, 0, 21),
-      Spending("22 - Short break+ ( 15 min )", 20, 0, 0, 22),
-      Spending("23 - Medium break ( 20 min )", 25, 0, 0, 23),
-      Spending("24 - Medium break+ ( 30 min )", 35, 0, 0, 24),
-      Spending("25 - The break ( 1 hour )", 60, 0, 0, 25),
-  };
+  introduceTheGame();
 
   runGame();
 
@@ -112,13 +118,6 @@ bool checkTxtFile(const string &txtFilename)
 {
   ifstream txtFile(txtFilename);
   return txtFile.good();
-}
-
-void introduceTheGame()
-{
-  cout << "Welcome to the Study Game!" << endl;
-  cout << "We're thrilled to have you join our learning adventure. This game is designed to make studying more engaging and rewarding. Here's how it works:" << endl;
-  cout << "In the Study Game, you can earn rewards for various study-related activities. These rewards include coins, diamonds, and rubies. The more you study and engage with learning materials, the more you earn. Here’s a quick look at how you can earn rewards:" << endl;
 }
 
 void createTxtFile()
@@ -162,7 +161,37 @@ void readTxtFile()
   txtFile.close();
 }
 
-void runGame() {}
+void introduceTheGame()
+{
+  cout << endl
+       << "Welcome to the Study Game, " << userName << "!!!" << endl;
+  cout << "We're thrilled to have you join our learning adventure. This game is designed to make studying more engaging and rewarding. Here's how it works:" << endl;
+  cout << "Enter a number, if the number corresponds to an activity: gain resources. If the number corresponds to a shop item: spend resources and acquire the item." << endl;
+  cout << "----- To exit the game: type '99' -----" << endl
+       << endl;
+}
+
+void runGame()
+{
+  cout << "*************** STUDY GAME ***************" << endl
+       << endl;
+  cout << "*************** Your activities: " << endl
+       << endl;
+
+  for (const Earning &i : earningGroup)
+  {
+    i.introduceItself();
+  }
+
+  cout << endl
+       << "*************** Store: " << endl
+       << endl;
+
+  for (const Spending &i : spendingGroup)
+  {
+    i.introduceItself();
+  }
+}
 
 void reWriteTxtFile()
 {
